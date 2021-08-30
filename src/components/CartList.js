ab1899f470b938325bed12bf3aa22cac0f548fba
AgungBK <agungbudikusuma1@gmail.com>
2021-08-30 23:03:56 +0700

    /

---
+++ src/components/CartList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+export default function CartColumns() {
+    return (
+        <div>
+            Ini cart list
+            <CartItem>
+
+            </CartItem>
+        </div>
+    );
+};
